6941ffe8f95df9c4eda4d38add002a9214d6e96c
Jhonatas Rodrigues Correa <jhonatasrcorrea@gmail.com>
2022-10-10 15:20:31 -0300

    Atualizações (Insert, SelectAll, SelectOne, Update, Delete)

--- AulaEF_CF/Program.cs
+++ AulaEF_CF/Program.cs
@@ -1,11 +1,9 @@
-﻿using AulaEF_CF.Context;
+﻿using System;
+using AulaEF_CF.Context;
 using AulaEF_CF.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity.Migrations;
 
 namespace AulaEF_CF
 {
@@ -28,31 +26,61 @@ namespace AulaEF_CF
                 context.Persons.Add(p);
                 context.SaveChanges();
                 #endregion
+
                 #region LISTA TODOS
+                Console.WriteLine("### SelectAll ###");
+                Console.WriteLine("-----------------");
                 var persons = new PersonContext().Persons.ToList();
                 foreach (var item in persons)
                 {
                     Console.WriteLine(item.ToString());
                 }
                 #endregion
+
                 #region LISTA UNICO
+                Console.WriteLine("### Select One ###");
+                Console.WriteLine("--------------");
+                Console.WriteLine("Insira um nome: ");
                 string n = Console.ReadLine();
                 //Person xpto = new PersonContext().Persons.Find(args[0]);
                 Person find = new PersonContext().Persons.FirstOrDefault(f => f.Name == n);
                 if (find != null)
+                {
                     Console.WriteLine(find.ToString());
+                    Console.WriteLine("[Pressione p/ Prosseguir]");
+                    Console.ReadKey();
+                }
                 else
+                {
                     Console.WriteLine("Registro não encontrado");
+                    Console.WriteLine("[Pressione p/ Prosseguir]");
+                    Console.ReadKey();
+                }
                 #endregion
+
+                #region ATUALIZAR
+                Console.WriteLine("### Atualizar ###");
+                Console.WriteLine("--------------");
+                find.Mobile = "UpdateSucess";
+                context.Entry(find).State = EntityState.Modified;
+                context.Persons.AddOrUpdate(find);
+                context.SaveChanges();
+                Console.WriteLine(find.ToString());
+                Console.WriteLine("[Pressione p/ Prosseguir]");
+                Console.ReadKey();
+                #endregion
+
                 #region REMOVE
+                Console.WriteLine("### Remove ###");
+                Console.WriteLine("--------------");
                 context.Entry(find).State = EntityState.Deleted;
                 context.Persons.Remove(find);
                 context.SaveChanges();
-                #endregion
-
+                Console.WriteLine("Removido com sucesso!!!");
+                Console.WriteLine("[Pressione p/ Prosseguir]");
                 Console.ReadKey();
+                #endregion                
             }
-
         }
     }
 }
